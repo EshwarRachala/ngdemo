d360e47dc61ea0c331c14cdccfcdc4b40908d54a
eshwar rachala <eshwar.appdev@gmail.com>
2017-04-22 09:34:36 -0400

    added route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
+import { HomeComponent } from './home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CreateComponent } from './create/create.component';
 
+const RouteModule = RouterModule.forRoot(
+  [{
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'create',
+    component: CreateComponent
+  }]
+);
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/create/create.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create',
+  template: '<h1>Create </h1>'
+})
+export class CreateComponent {
+}
