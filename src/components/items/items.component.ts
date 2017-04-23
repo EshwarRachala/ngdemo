f9c8e9f4b96ac1304bbd70d885ed3cb4c80c9019
eshwar rachala <eshwar.appdev@gmail.com>
2017-04-23 08:37:20 -0400

    updated

--- src/components/items/items.component.ts
+++ src/components/items/items.component.ts
@@ -5,31 +5,13 @@ import { ItemsService } from '../../services/items.service';
 import { AppStore } from '../../models/appstore.model';
 import { Item } from '../../models/item.model';
 import { ItemsList } from '../items-list/items-list.component';
-import { ItemDetail } from'../item-detail/item-detail.component'
+import { ItemDetail } from '../item-detail/item-detail.component'
 
 @Component({
   selector: 'items',
-  template: `
-  <div class="mdl-grid items">
-    <div class="mdl-cell mdl-cell--6-col">
-      <items-list [items]="items | async"
-        (selected)="selectItem($event)" (deleted)="deleteItem($event)">
-      </items-list>
-    </div>
-    <div class="mdl-cell mdl-cell--6-col">
-      <item-detail
-        (saved)="saveItem($event)" (cancelled)="resetItem($event)"
-        [item]="selectedItem | async">Select an Item</item-detail>
-    </div>
-  </div>
-  `,
-  styles: [`
-    .items {
-      padding: 20px;
-    }
-  `],
-  providers: [ItemsService],
- // directives: [ItemsList, ItemDetail]
+  templateUrl: './items.componentn.html',
+  styleUrls: ['./items.component.css'],
+  providers: [ItemsService]
 })
 export class Items {
   items: Observable<Array<Item>>;
@@ -40,7 +22,6 @@ export class Items {
     this.items = itemsService.items;
     this.selectedItem = store.select('selectedItem');
     this.selectedItem.subscribe(v => console.log(v));
-
 
     itemsService.loadItems();
   }
@@ -56,17 +37,11 @@ export class Items {
 
   saveItem(item: Item) {
     this.itemsService.saveItem(item);
-
-    // Generally, we would want to wait for the result of `itemsService.saveItem`
-    // before resetting the current item.
     this.resetItem();
   }
 
   deleteItem(item: Item) {
     this.itemsService.deleteItem(item);
-
-    // Generally, we would want to wait for the result of `itemsService.deleteItem`
-    // before resetting the current item.
     this.resetItem();
   }
 }
