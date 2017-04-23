9b5334e9e5f31e1f9b714ab454cc9a375b640af5
eshwar rachala <eshwar.appdev@gmail.com>
2017-04-23 10:28:48 -0400

    updated

--- src/app/container/change/change.component.ts
+++ src/app/container/change/change.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-change',
-  template: '<h1>hi from chnage</h1>'
+  templateUrl: './change.component.html'
 })
 export class ChangeComponent {
 
